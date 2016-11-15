2669dbb6f7897c5effb9d4b653357b4065bb5f32
jomphp <gwbiz4u@gmail.com>
2016-11-15 16:12:13 +0800

    Update senarai

--- resources/views/files/senarai.blade.php
+++ resources/views/files/senarai.blade.php
@@ -1,12 +1,12 @@
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+@extends('layouts.app')
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
+@section('content')
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<div class="panel panel-default">
+<div class="panel-heading">Senarai Files</div>
+<div class="panel-body">
 <table class="table">
   <thead>
     <tr>
@@ -17,7 +17,13 @@
   <tbody>
     <tr>
       <td>ID</td>
-      <td></td>
+      <td>Nama File</td>
     </tr>
   </tbody>
 </table>
+</div>
+</div>
+</div>
+</div>
+</div>
+@endsection
