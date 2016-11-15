363d4220926d9363264cb58b79d47c9867adc5e0
jomphp <gwbiz4u@gmail.com>
2016-11-15 16:05:55 +0800

    Contoh Form File

---
+++ resources/views/files/borang_tambah.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<div class="panel panel-default">
+<div class="panel-heading">Borang Tambah File</div>
+<div class="panel-body">
+<form method="POST" action="{{ url()->current() }}">
+  {{ csrf_field() }}
+
+  <div class="form-group">
+      <label>File</label>
+      <input type="file" name="nama_file">
+  </div>
+
+  <div class="form-group">
+      <label>Aktiviti</label>
+      <select name="aktiviti" class="form-control">
+        <option value="kemaskini_lokaliti">Kemaskini Lokaliti</option>
+        <option value="belah_bahagi">Belah Bahagi</option>
+      </select>
+  </div>
+
+  <div class="form-group">
+      <label>Tahun</label>
+      <select name="tahun" class="form-control">
+        <option value="kemaskini_lokaliti">2016</option>
+      </select>
+  </div>
+
+  <div class="form-group">
+      <label>Negeri</label>
+      <input type="text" name="negeri" value="Putrajaya" class="form-control" readonly="readonly">
+  </div>
+
+  <div class="form-group">
+      <label>Sukuan</label>
+      <select name="sukuan" class="form-control">
+        <option value="s1">S1</option>
+        <option value="s2">S2</option>
+        <option value="s3">S3</option>
+        <option value="dpi">DPI</option>
+      </select>
+  </div>
+
+  <div class="form-group">
+      <label>Penggal</label>
+      <select name="sukuan" class="form-control">
+        <option value="bp">6 Bulan Pertama</option>
+        <option value="bt">6 Bulan Terakhir</option>
+      </select>
+  </div>
+
+  <div class="form-group">
+      <label>Status Belah Bahagi</label>
+      <select name="sukuan" class="form-control">
+        <option value="sebelum">Sebelum</option>
+        <option value="selepas">Selepas</option>
+      </select>
+  </div>
+
+  <div class="form-group">
+    <button type="submit" class="btn btn-primary">
+        Upload File
+    </button>
+  </div>
+</form>
+</div>
+</div>
+</div>
+</div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,19 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
-|
-*/
-
+// Route paparan halaman utama
 Route::get('/', function () {
     // laravel cari folder resources/views/welcome.blade.php
     return view('welcome');
 });
 
+// Route login dan password reset
+// Generated oleh Laravel php artisan make:auth
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+// Route paparan halaman dashboard
+Route::get('/dashboard', 'HomeController@index');
 
-
+// Cara penggunaan routes yang mempunyai prefix
+// yang sama (contoh: users) tanpa menggunakan Route::group()
 // http://sistemdata.dev/users
 // Route::get('users', function() {
 //   echo 'Senarai users';
@@ -32,7 +25,7 @@ Route::get('/home', 'HomeController@index');
 // });
 
 
-// Routes untuk pengurusan users
+// Routes group untuk prefix users
 Route::group(['prefix' => 'users'], function() {
 
     // Papar senarai users bagi alamat http://sistemdata.dev/users
@@ -71,6 +64,46 @@ Route::group(['prefix' => 'users'], function() {
     // alamat http://sistemdata.dev/users/{id}
     Route::delete('{id}', function($id) {
       return 'Berjaya delete user ' . $id;
+    });
+
+});
+
+
+// Routes group untuk prefix files
+Route::group(['prefix' => 'files'], function() {
+
+    // Papar senarai files bagi alamat http://sistemdata.dev/files
+    Route::get('/', function() {
+      // Paparkan fail template bernama senarai.php dari
+      // folder resources/views/files
+      return view('files/senarai');
+    });
+
+    // Papar borang tambah file bagi alamat http://sistemdata.dev/files/tambah
+    Route::get('tambah', function() {
+      return view('files/borang_tambah');
+    });
+
+    // Terima data dari HTTP POST dari alamat http://sistemdata.dev/files/tambah
+    Route::post('tambah', function() {
+      return 'Proses data simpan ke database';
+    });
+
+    // Papar borang edit files bagi alamat http://sistemdata.dev/files/{id}/edit
+    Route::get('{id}/edit', function($id) {
+      return view('files/borang_edit');
+    });
+
+    // Terima data dari HTTP POST dari alamat http://sistemdata.dev/files/{id}/edit
+    // Untuk kemaskini data berdasarkan ID
+    Route::patch('{id}/edit', function($id) {
+      return 'Update data file ' . $id;
+    });
+
+    // Hapus rekod user daripada database berdasarkan
+    // alamat http://sistemdata.dev/files/{id}
+    Route::delete('{id}', function($id) {
+      return 'Berjaya delete file ' . $id;
     });
 
 });
